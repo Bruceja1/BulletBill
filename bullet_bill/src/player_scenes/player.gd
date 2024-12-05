7b4467be063848acca2a78c130cb0bee74fc551f
Bruceja <2103932jagessar@zuyd.nl>
2024-12-05 20:53:55 +0100

    Added player rotation on key press

--- bullet_bill/src/player_scenes/player.gd
+++ bullet_bill/src/player_scenes/player.gd
@@ -3,6 +3,7 @@ var screen_size
 signal hit
 
 var speed = 1000
+var max_rotation = 30
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
@@ -12,12 +13,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO
 	
+	rotation_degrees = 0
+	
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= speed
+		rotation_degrees = -max_rotation
+		
 		
 	if Input.is_action_pressed("move_down"):
 		velocity.y += speed
+		rotation_degrees = max_rotation
 		
+				
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
 
