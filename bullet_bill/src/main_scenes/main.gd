55e43c049e0ca90f6ae9d0f0392124d11815193a
Bruceja <2103932jagessar@zuyd.nl>
2024-12-05 13:50:08 +0100

    Objects spawn randomly

--- bullet_bill/src/main_scenes/main.gd
+++ bullet_bill/src/main_scenes/main.gd
@@ -1,6 +1,15 @@
 extends Node
-@export var object_scene: PackedScene
-var obstacle_position = Vector2(1200, 608)
+
+var obstacle_scenes = [
+	preload("res://src/obstacle1_scenes/obstacle1-1.tscn"),
+	preload("res://src/obstacle1_scenes/obstacle1-2.tscn"),
+	preload("res://src/obstacle1_scenes/obstacle1-3.tscn"),
+	preload("res://src/obstacle1_scenes/obstacle1-4.tscn"),
+	preload("res://src/obstacle1_scenes/obstacle1-5.tscn")
+]
+
+#@export var object_scene: PackedScene
+var obstacle_position = Vector2(1600, 608)
 var obstacle_scale = Vector2(5, 5)
 var obstacle_velocity = Vector2(2000, 0)
 
@@ -15,7 +24,9 @@ func _process(delta: float) -> void:
 
 
 func _on_obstacle_timer_timeout() -> void:
-	var obstacle = object_scene.instantiate()
+	#var obstacle = object_scene.instantiate()
+	var obstacle = obstacle_scenes[randi() % obstacle_scenes.size()].instantiate()	
+	
 	obstacle.position = obstacle_position
 	obstacle.position.y = obstacle.spawn_height
 	obstacle.set("scale", obstacle_scale)
